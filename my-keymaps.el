c93618dec236b66308463fc6840fe82bb4717998
Arturo Pie <arturotd08@yahoo.ca>
2012-01-12 00:02:43 -0500

    binds to smex since that one shows you the options whereas you type the command

--- my-keymaps.el
+++ my-keymaps.el
@@ -23,8 +23,8 @@
 (global-set-key (kbd "C-'") 'forward-word)
 
 ;; from effective emacs: http://sites.google.com/site/steveyegge2/effective-emacs
-(global-set-key "\C-x\C-m" 'execute-extended-command)
-(global-set-key "\C-c\C-m" 'execute-extended-command)
+(global-set-key "\C-x\C-m" 'smex)
+(global-set-key "\C-c\C-m" 'smex)
 (global-set-key "\C-w" 'backward-kill-word)
 (global-set-key "\C-x\C-k" 'kill-region)
 (global-set-key "\C-c\C-k" 'kill-region)
