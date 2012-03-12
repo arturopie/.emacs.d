739476f3ff8e0d046050e85a96dd8004ce490cee
Arturo Pie <arturotd08@yahoo.ca>
2012-03-11 23:43:45 -0400

    Updates my-defuns. Adds find-blueprint, send-current-line-to-next-window, new-line-in-normal-mode, extract-variable, etc

--- my-defuns.el
+++ my-defuns.el
@@ -1,29 +1,115 @@
-;;; no worky
+(defun send-current-line-to-next-window ()
+  "Send current line to next window"
+  (interactive)
+  (let ((current-line (buffer-substring-no-properties (point-at-bol) (point-at-eol)))
+        (target (window-buffer (next-window))))
+    (with-current-buffer target
+      (insert current-line))))
+
+(defun new-line-in-normal-mode ()
+  "make a new line without moving the cursor or leaving normal mode"
+  (interactive)
+  (evil-set-marker ?z)
+  (evil-insert-newline-below)
+  (evil-force-normal-state)
+  (evil-goto-mark ?z))
+
+(defun extract-variable ()
+  (interactive)
+  (let ((name (read-from-minibuffer "Variable name: ")))
+    (evil-change (region-beginning) (region-end))
+    (insert name)
+    (evil-open-above 1)
+    (insert (concat name " ="))
+    (evil-paste-after 1))
+    (indent-for-tab-command)
+  (evil-normal-state))
+
+
 (defun inline-variable ()
   (interactive)
   (let ((name (current-word)))
     (re-search-forward "= ")
     (let ((value (buffer-substring (point) (point-at-eol))))
       (kill-whole-line)
-      (search-forward value)
-      (replace-match name))))
+      (search-forward name)
+      (replace-match value)))
+  (evil-normal-state))
+
+;; Foo = 1.4 * whatever()
+;; blah.blah(foo)
+(defvar rails/models-alist nil)
+(defun rails-model-files ()
+  (all-files-under-dir-recursively (concat (eproject-root) "app/models") ".rb$"))
+
+(defun rails-models-alist ()
+  (or rails/models-alist
+      (setq rails/models-alist (mapcar 'rails-class-and-table-name
+                                       (rails-model-files)))))
+
+(defun rails-models ()
+  (mapcar 'car (rails-models-alist)))
+
+(defun rails-class-and-table-name (file-name)
+  (let ((class (rails-class-from-file-name file-name))
+        (table (rails-table-name-from-file-name file-name)))
+    `(,class . ,table)))
+
+(defun is-rails-model-p ()
+  (let ((model-regexp (concat "^" (eproject-root) "app/models")))
+    (string-match model-regexp (buffer-file-name))))
+
+(defun rails-table-name-from-file-name (file-name &optional ns)
+  "get an underscored version of the current models name, passing in what to use as namespace delimiter"
+  (let* ((delim-with (or ns "_"))
+         (model-dir (concat (eproject-root) "app/models/"))
+         (model (replace-regexp-in-string model-dir "" file-name))
+         (filename (replace-regexp-in-string "/" delim-with model)))
+    (replace-regexp-in-string ".rb$" "" filename)))
+
+(defun rails-class-from-file-name (file-name)
+  (let* ((table-name (rails-table-name-from-file-name file-name "::"))
+         (capitalized (capitalize table-name)))
+    (replace-regexp-in-string "_" "" capitalized)))
+
+(defun rails-prompt-for-model ()
+  (let* ((model (rails-class-from-file-name (buffer-file-name)))
+         (initial-value (if (is-rails-model-p) model))
+         (input (ido-completing-read "Model: " (rails-models) nil t initial-value)))
+    (if (string= "" input) model input)))
+
+(defun rails-table-name-for-model (model)
+  (pluralize-string (cdr (assoc model rails/models-alist))))
+
+(defun find-blueprint ()
+  (interactive)
+  (let* ((root (eproject-root))
+         (target (rails-prompt-for-model))
+         (search (concat "^" target ".blueprint")))
+    (find-file (concat root "test/blueprints.rb"))
+    (or (re-search-forward search nil t)
+        (re-search-backward search nil t))))
 
 (defun schema ()
   (interactive)
 
-  (let* ((name (replace-regexp-in-string ".rb$" "s" (buffer-name)))
+  (let* ((name (rails-table-name-for-model (rails-prompt-for-model)))
          (root (eproject-root))
          (regexp (concat "create_table \"" name "\"")))
+
     (find-file (concat root "db/schema.rb"))
-    (or
-     (re-search-forward regexp nil t)
-     (re-search-backward regexp nil t))
+    (or (re-search-forward regexp nil t)
+        (re-search-backward regexp nil t))
     (message (concat "looking for " name))))
 
 (defun format-json ()
   (interactive)
   (let ((cmd "python -mjson.tool"))
     (shell-command-on-region (region-beginning) (region-end) cmd nil t)))
+
+(defun copy-to-end-of-line ()
+  (interactive)
+  (evil-yank (point) (point-at-eol)))
 
 (defun command-t ()
   (interactive)
@@ -62,13 +148,22 @@
   (interactive)
   (message "building project tags")
   (let ((root (eproject-root)))
-    (shell-command (concat "ctags -e -R --exclude=db --exclude=test -f " root "TAGS " root)))
+    (shell-command (concat "ctags -e -R -L --verbose --excmd=n --extra=+fq --fields=+afiKlmnsSzt --file-scope=no --exclude=db --exclude=test --exclude=.git --exclude=public -f " root "TAGS " root)))
   (visit-project-tags)
   (message "tags built successfully"))
 
+(defun build-gtags ()
+  (interactive)
+  (message "building gtags")
+  (let ((root (eproject-root)))
+    (shell-command (concat "(cd " root " && gtags)"))
+    (message "tags loaded")))
+
 (defun my-find-tag ()
   (interactive)
-  (visit-project-tags)
+  (if (file-exists-p (concat (eproject-root) "TAGS"))
+      (visit-project-tags)
+    (build-ctags))
   (etags-select-find-tag-at-point))
 
 (defun visit-project-tags ()
@@ -77,29 +172,32 @@
     (visit-tags-table tags-file)
     (message (concat "Loaded " tags-file))))
 
+(defun set-relative-shoulda-command ()
+  (setq shoulda-command (concat "(cd " (eproject-root) " && ruby \"%f\" %o)")))
+
 (defun test-verify ()
   (interactive)
   (if (eproject-attribute :use-shoulda)
       (progn
-        (set-shoulda-command-to-proj-root)
+        (set-relative-shoulda-command)
         (shoulda-verify))
-      (rspec-verify)))
+    (rspec-verify)))
 
 (defun test-verify-all ()
   (interactive)
   (if (eproject-attribute :use-shoulda)
       (progn
-        (set-shoulda-command-to-proj-root)
+        (set-relative-shoulda-command)
         (shoulda-verify-all))
-      (rspec-verify-all)))
+    (rspec-verify-all)))
 
 (defun test-verify-single ()
   (interactive)
   (if (eproject-attribute :use-shoulda)
       (progn
-        (set-shoulda-command-to-proj-root)
+        (set-relative-shoulda-command)
         (shoulda-verify-single))
-      (rspec-verify-single)))
+    (rspec-verify-single)))
 
 (defun test-toggle ()
   (interactive)
