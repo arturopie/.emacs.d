b3927f319981fe702431660e69577fd9fe24686a
Arturo Pie <arturotd08@yahoo.ca>
2012-04-14 16:51:47 -0400

    makes C-j to use ace-jump-word-mode in ruby mode as well

--- init/init-ace-jump-mode.el
+++ init/init-ace-jump-mode.el
@@ -1,5 +1,7 @@
 (require 'ace-jump-mode)
 (global-set-key (kbd "C-j") 'ace-jump-word-mode)
+(define-key ruby-mode-map (kbd "C-j") 'ace-jump-word-mode)
+
 (global-set-key (kbd "C-c j") 'ace-jump-char-mode)
 
 (provide 'init-ace-jump-mode)
--- my-keymaps.el
+++ my-keymaps.el
@@ -42,10 +42,10 @@
             (define-key ido-completion-map "\C-n" 'ido-next-match)
             (define-key ido-completion-map "\C-p" 'ido-prev-match)))
 
-(add-hook 'ruby-mode-hook
-          (lambda ()
-            (local-set-key (kbd "RET") 'newline-and-indent)
-            (local-set-key [f1] 'yari)))
+;; (add-hook 'ruby-mode-hook
+;;           (lambda ()
+;;             (local-set-key (kbd "RET") 'newline-and-indent)
+;;             (local-set-key [f1] 'yari)))
 
 (add-hook 'change-major-mode-hook
           (lambda ()
