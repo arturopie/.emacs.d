365447ae1b0e7895484b60f33674b61d5b908525
Arturo Pie <arturop@nulogy.com>
2014-02-03 15:14:55 -0500

    removes evil-insert-state from insert-ticket-number-from-branch-name

--- init/init-magit.el
+++ init/init-magit.el
@@ -6,7 +6,6 @@
 
 (defun insert-ticket-number-from-branch-name ()
   (erase-buffer)
-  (evil-insert-state)
   (let* ((current-branch (car (vc-git-branches)))
          (ticket-number (replace-regexp-in-string "[0-9]+\\(_.*\\)$" ""
                                                   current-branch nil nil 1)))
